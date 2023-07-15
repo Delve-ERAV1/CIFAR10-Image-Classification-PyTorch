f753f5805a5be1d43f63ee66b3ab3edcb5572e2d
Sijuade Oguntayo <cydalsij@outlook.com>
2023-07-15 16:25:37 +0100

    Update augment.py

--- augment/augment.py
+++ augment/augment.py
@@ -9,7 +9,8 @@ def get_transforms(means, stds):
   train_transforms = A.Compose(
       [
           A.Normalize(mean=means, std=stds, always_apply=True),
-          A.RandomCrop(height=32, width=32, pad=4, always_apply=True),
+          A.PadIfNeeded(min_height=36, min_width=36, always_apply=True),
+          A.RandomCrop(height=32, width=32, always_apply=True),
           A.HorizontalFlip(),
           A.Cutout (fill_value=means),
           ToTensorV2(),
